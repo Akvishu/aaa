4eb60939b96c3388e2c12bce6242f7f3808952a1
Akvishu <abhichopan@gmail.com>
2020-07-04 14:01:26 +0530

    Akash

---
+++ WEB-INF/classes/Sub.java
@@ -0,0 +1,24 @@
+import javax.servlet.*;
+import java.io.* ;
+public class Sub extends GenericServlet{
+	
+	public void service(ServletRequest req,ServletResponse res){
+		PrintWriter out = null;
+		try{
+			res.setContentType("text/html");
+			out=res.getWriter();
+			String a = req.getParameter("num1");
+			String b = req.getParameter("num2");
+			int n1=Integer.parseInt(a);
+			int n2=Integer.parseInt(b);
+			int n;
+			n=n1-n2;
+			out.print("<html><body>");
+			out.print("<h1><center>My First Web App</center></h1>");
+			out.print("Sum of "+n1+" and "+n2+" :- "+n+" ");
+			out.print("</body></html>");
+		}catch(Exception e){
+		out.print(e);
+	}
+	}
+}
